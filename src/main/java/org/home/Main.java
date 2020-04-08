f3afd34bc85bf12005913e0523650a02b39aa927
Donaldhan <shaoqinghan@aliyun.com>
2020-04-08 15:41:28 +0800

    init project

--- src/main/java/org/home/Main.java
+++ src/main/java/org/home/Main.java
@@ -3,6 +3,7 @@ package org.home;
 import java.util.logging.Logger;
 
 /**
+ * http://localhost:8006/metrics/list
  * @ClassName: Main
  * @Description:
  * @Author: Donaldhan
@@ -12,7 +13,8 @@ public class Main {
     private final static Logger log = Logger.getLogger("org.home.Main");
     public static void main(String[] args) {
         log.info("start metrics...");
-        Bootstrap.init();
+//        Bootstrap.init();
+        Bootstrap.init("metrics-conf.properties");
         log.info("metrics servers is listen on 8006");
     }
 }
